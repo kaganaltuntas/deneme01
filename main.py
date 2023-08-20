4542b675e453aa38e16a4504e829f2ec5bd6c062
kaganaltuntas <142260835+kaganaltuntas@users.noreply.github.com>
2023-08-20 16:13:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("merhaba")
+import random
+secenek = []
+
+a = input("BİLGE RANDOM'a hoş geldiniz eklemek istediğiniz seçeneği yazın:")
+secenek.append(a)
+a = input("eklemek istediğiniz seçeneği yazın:")
+secenek.append(a)
+a = input("eklemek istediğiniz seçeneği yazın(bitirmek için ise '1')yazın:")
+if(a == 1):
+ print(random.choice(secenek))
+ a = input("eklemek istediğiniz seçeneği yazın(bitirmek için ise '1')yazın:")
+ if(a != 1):
+  a = input("eklemek istediğiniz seçeneği yazın(bitirmek için ise '1')yazın:")
+  while (a != 1):
+   print(secenek.append(a))
+a = input("eklemek istediğiniz seçeneği yazın(bitirmek için ise '1')yazın:")
